9c67cc93e2cb8ff27aa1c9e1d42170f3bace249c
unknown <icyatwandoba@gmail.com>
2024-08-05 04:30:14 -0700

    design

--- src/dashboard/components/session2.js
+++ src/dashboard/components/session2.js
@@ -11,12 +11,12 @@ function Session2() {
 
             <div className='updates'>
                 <div className='update'>
-                    <img src='' alt='' />
+                    <img src='https://img.freepik.com/free-vector/matcha-green-tea-powder-vector-realistic-product-placement-mock-up-healthy-drink-label-designs_1268-18117.jpg?uid=R102997587&ga=GA1.2.2142793496.1716934876&semt=ais_hybrid' alt='' />
                     <label>Shisha mugabo</label>
                 </div>
 
                 <div className='update'>
-                    <img src='' alt='' />
+                    <img src='https://img.freepik.com/free-vector/fresh-milk-bottle-ad_52683-35123.jpg?uid=R102997587&ga=GA1.2.2142793496.1716934876&semt=ais_hybrid' alt='' />
                     <label>Ubaye kabebeyi kweri</label>
                 </div>
 
@@ -24,6 +24,11 @@ function Session2() {
                     <img src='' alt='' />
                     <label>Zana amataye</label>
                 </div>
+            </div>
+
+            <div className='showMore'>
+                <label>Show more</label>
+                <img src='https://cdn-icons-png.flaticon.com/128/8213/8213522.png' alt='' />
             </div>
         </div>
     </div>
