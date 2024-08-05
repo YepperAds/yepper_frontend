cc210ee4721ce205b0b23253132187f043b780df
unknown <icyatwandoba@gmail.com>
2024-08-05 09:37:33 -0700

    design

--- src/dashboard/Home.js
+++ src/dashboard/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './styles/home.css'
 import Header from './header'
 import AddButton from './components/addButton'
+import Session2 from './components/session2'
 
 function Home() {
   return (
     <div className='dashboard-container'>
       <Header />
       <AddButton />
+      <Session2 />
     </div>
   )
 }
