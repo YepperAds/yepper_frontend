2e3e8f3f3fa9e31a965f3bef0a82367a6a47ad6d
icyatwa <icyatwandoba@gmail.com>
2025-10-29 16:29:31 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -17,8 +17,6 @@ import VerifySuccess from './pages/VerifySuccess';
 import VerifyError from './pages/VerifyError';
 import EmailVerification from './pages/EmailVerificationPending';
 import CheckEmail from './pages/CheckEmail';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
 
 // AdPromoter
 import WebsiteCreation from './AdPromoter/pages/websiteCreation';
@@ -84,8 +82,6 @@ function App() {
               <Route path="/payment/callback" element={<PaymentCallback />} />
               <Route path="/add-website" element={<UnifiedWebsiteCreation />} />
               <Route path="/advertise" element={<UnifiedAdUploadFlow />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password/:token" element={<ResetPassword />} />
 
               {/* AdPromoter */}
               <Route path="/websites" element={
